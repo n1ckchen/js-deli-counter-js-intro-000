9441eadd5105858e064ba22f8c0928ea066cbc8b
nick chen <nick.chen.pro@gmail.com>
2020-03-15 18:14:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-    var output = [];
+    var finalLine = [];
     if (katzDeliLine.length > 0) {
       for(var i = 0; i < katzDeliLine.length; i++) {
-        output += (i + 1) + ". " + katzDeliLine[i] + ", ";
+        finalLine += (i + 1) + ". " + katzDeliLine[i] + ", ";
       }
-      output = output.slice(0, output.length - 2);
+      finalLine = finalLine.slice(0, finalLine.length - 2);
       return "The line is currently: " + output;
     } else {
         return "The line is currently empty.";
