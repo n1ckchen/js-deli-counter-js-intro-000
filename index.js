a7db8e86f965d5fb525b6fcca6eccfa189654ab5
nick chen <nick.chen.pro@gmail.com>
2020-03-15 18:21:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(katzDeliLine) {
         output += (i + 1) + ". " + katzDeliLine[i] + ", ";
       }
       output = output.slice(0, output.length - 2);
-      return 'The line is currently: '' + output;
+      return 'The line is currently: ' + output;
     } else {
         return 'The line is currently empty.'';
     }
